10d8fd0c8ff689d76b4e60d09c51fd82e636b21a
Eddie <eocampo52@gmail.com>
2024-12-11 22:05:32 -0600

    look over evernote notes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route     GET api/users
-// @desc      Test route
+const User = require('../models/User')
+
+// @route     POST api/users
+// @desc      Register user
 // @access    Public
 
-router.get('/', (req, res) => {
-  res.send('User route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please inlcude a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+const {name, email, password} = req.body
+
+try {
+  // See if user exists
+
+    // Get user's gravatar
+
+    // Encrypt password
+
+    // Return jsonwebtoken
+} catch (err) {
+
+}
+    
+
+    res.send('User route');
+  }
+);
 
 module.exports = router;
