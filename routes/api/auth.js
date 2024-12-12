eaa73b8f19504974f6fe519775837c907bb9d3bd
Eddie <eocampo52@gmail.com>
2024-12-12 11:36:18 -0600

    profile schema added. create profile api successfully. look over evernote notes 12.12

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth')
+const bcrypt = require('bcryptjs');
+const auth = require('../../middleware/auth');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 
-const User = require('../../models/User')
+const User = require('../../models/User');
 
 // @route     GET api/auth
 // @desc      Auth route
 // @access    Public
 
 router.get('/', auth, async (req, res) => {
-try {
-const user = await User.findById(req.user.id).select('-password')
-res.json(user)
-} catch (err) {
-console.error(err.message)
-res.status(500).send('Server Error')
-}
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     POST api/auth
@@ -29,10 +30,7 @@ router.post(
   '/',
   [
     check('email', 'Please inlcude a valid email').isEmail(),
-    check(
-      'password',
-      'Password is required'
-    ).exists(),
+    check('password', 'Password is required').exists(),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -52,8 +50,14 @@ router.post(
       }
       // Return jsonwebtoken
 
+      const isMatch = await bcrypt.compare(password, user.password);
 
-      
+      if(!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
       const payload = {
         user: {
           id: user.id,
@@ -61,10 +65,13 @@ router.post(
       };
       jwt.sign(
         payload,
-        config.get('jwtSecret'), { expiresIn:3600 }, (err, token) => {
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
           if (err) throw err;
           res.json({ token });
-        })
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
